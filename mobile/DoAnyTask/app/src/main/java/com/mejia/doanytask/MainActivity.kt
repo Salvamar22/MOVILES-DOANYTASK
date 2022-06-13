0bb42b215af47585e70e70abc690666906da11c3
ManuelMadriz <00026220@gmail.com>
2022-06-13 17:06:30 -0600

    Activities fragments layout design

--- mobile/DoAnyTask/app/src/main/java/com/mejia/doanytask/MainActivity.kt
+++ mobile/DoAnyTask/app/src/main/java/com/mejia/doanytask/MainActivity.kt
@@ -2,8 +2,10 @@ package com.mejia.doanytask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
