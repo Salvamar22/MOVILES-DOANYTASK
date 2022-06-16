9eee73ff083ce32f03e340859cce6cedb2f50a68
ManuelMadriz <00026220@gmail.com>
2022-06-15 21:07:18 -0600

    Add ActivityList and Monthly funcionality

--- mobile/DoAnyTask/app/src/main/java/com/mejia/doanytask/ActivityListFragment.kt
+++ mobile/DoAnyTask/app/src/main/java/com/mejia/doanytask/ActivityListFragment.kt
@@ -9,8 +9,12 @@ import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.mejia.doanytask.databinding.FragmentActivityListBinding
+import com.mejia.doanytask.databinding.ItemActivity2Binding
+import com.mejia.doanytask.databinding.ItemActivityBinding
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class ActivityListFragment : Fragment() {
 
@@ -21,7 +25,111 @@ class ActivityListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_activity_list, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_activity_list, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val acts = List<Activity>(6) { it ->
+            Activity(
+                "Evaluación práctica ${it + 1}.\n" +
+                        "Uca. Presentacion\n" +
+                        "Diseño App",
+                "alta",
+                LocalDateTime.of(2022, it + 2, 5, 14, 30)
+            )
+        }
+
+        val activityRecyclerView = binding.activityListRecyclerView
+        val activityAdapter = ActivityAdapter()
+        activityRecyclerView.apply {
+            adapter = activityAdapter
+        }
+        activityAdapter.setData(acts, 1)
+    }
 }
+
+    data class Activity (
+        val description: String,
+        val priority: String,
+        val date: LocalDateTime
+    ) {
+        public fun getHour(): String {
+            return date.format(DateTimeFormatter.ofPattern("hh:mm"))
+        }
+        public fun getDate(): String {
+            return date.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"))
+        }
+    }
+
+    open class ActivityAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        private val ITEM_TYPE_DEFAULT = 0
+        private val ITEM_TYPE_ALTERNATIVE = 1
+
+        private var activityList: List<Activity>? = null
+        private var typeItem: Int = ITEM_TYPE_DEFAULT
+
+        fun setData(data: List<Activity>, type: Int = ITEM_TYPE_DEFAULT) {
+            this.activityList = data
+            typeItem = type
+            notifyDataSetChanged()
+        }
+
+        inner class ActivityViewHolder(private val binding: ItemActivityBinding) : RecyclerView.ViewHolder(binding.root) {
+            fun bind(activity: Activity) {
+                binding.activity = activity
+                //binding.textHour.text = activity.getHour()
+                binding.textDate.text = activity.getDate()
+                binding.executePendingBindings()}
+
+        }
+
+        inner class ActivityViewHolder2(private val binding: ItemActivity2Binding) : RecyclerView.ViewHolder(binding.root) {
+            fun bind(activity: Activity) {
+                binding.activity = activity
+                binding.textHour.text = activity.getHour()
+                binding.activityDayWeek.text = activity.date.format(DateTimeFormatter.ofPattern("dd"))
+                binding.activityMonth.text = activity.date.format(DateTimeFormatter.ofPattern("MMM"))
+                binding.activityDayMonth.text = activity.date.format(DateTimeFormatter.ofPattern("dd"))
+                binding.executePendingBindings()}
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            when(typeItem) {
+                ITEM_TYPE_ALTERNATIVE -> {
+                    ActivityViewHolder2(
+                        DataBindingUtil.inflate(
+                            LayoutInflater.from(parent.context),
+                            R.layout.item_activity2,
+                            parent,
+                            false
+                        )
+                    )
+                }
+                else -> {
+                    ActivityViewHolder(
+                        DataBindingUtil.inflate(
+                            LayoutInflater.from(parent.context),
+                            R.layout.item_activity,
+                            parent,
+                            false
+                        )
+                    )
+                }
+            }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            activityList?.let {
+                when(typeItem) {
+                    ITEM_TYPE_ALTERNATIVE -> (holder as ActivityViewHolder2).bind(it[position])
+                    else -> (holder as ActivityViewHolder).bind(it[position])
+                }
+            }
+        }
+
+        override fun getItemCount() = activityList?.size ?: 0
+    }
--- mobile/DoAnyTask/app/src/main/java/com/mejia/doanytask/MonthlyFragment.kt
+++ mobile/DoAnyTask/app/src/main/java/com/mejia/doanytask/MonthlyFragment.kt
@@ -9,8 +9,14 @@ import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mejia.doanytask.databinding.CalendarCellBinding
 import com.mejia.doanytask.databinding.FragmentMonthBinding
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.WeekFields
+import java.util.*
 
 class MonthlyFragment : Fragment() {
 
@@ -24,4 +30,105 @@ class MonthlyFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_month, container, false)
         return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        selectedDate = LocalDate.now()
+        setMonthView()
+        binding.actionNextMonth.setOnClickListener { actionNextMonth(it)}
+        binding.actionPreviusMonth.setOnClickListener { actionPreviusMonth(it)}
+    }
+
+    private fun setMonthView() {
+        binding.monthYearText.text = monthYearFromDate(selectedDate)
+        val daysInMonth: List<String> = daysInMonthList(selectedDate)
+
+        val calendarRecyclerView = binding.calendarRecyclerView
+        val calendarAdapter = CalendarAdapter()
+
+        calendarRecyclerView.layoutManager = GridLayoutManager(context, 7)
+        calendarRecyclerView.apply {
+            adapter = calendarAdapter
+        }
+        calendarAdapter.setData(daysInMonth)
+
+    }
+
+    private fun daysInMonthList(selectedDate: LocalDate): List<String> {
+        /*val yearMonth: YearMonth = YearMonth.from(selectedDate)
+        val daysInMonth = yearMonth.lengthOfMonth()
+        val dayOfWeek
+        selectedDate.*/
+
+        val calenderDay: LocalDate = selectedDate.withDayOfMonth(1).with(WeekFields.of(Locale.US).dayOfWeek(), 1)
+
+        val daysInMonthList = List<String>(42) {
+            val date = calenderDay.plusDays( it.toLong());
+            date.dayOfMonth.toString()
+        }
+
+        return daysInMonthList
+    }
+
+    private fun monthYearFromDate(date: LocalDate): String {
+        return date.format(DateTimeFormatter.ofPattern("MMM yyyy"))
+    }
+
+    private fun actionPreviusMonth(view: View) {
+        selectedDate = selectedDate.plusMonths(-1)
+        setMonthView()
+    }
+
+    private fun actionNextMonth(view: View) {
+        selectedDate = selectedDate.plusMonths(1)
+        setMonthView()
+    }
+}
+
+
+class CalendarAdapter : RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
+
+    private var daysOfMonth: List<String>? = null
+    fun setData(data: List<String>) {
+        this.daysOfMonth = data
+        notifyDataSetChanged()
+    }
+
+    inner class CalendarViewHolder(private val binding: CalendarCellBinding) : RecyclerView.ViewHolder(binding.root) {
+        /*
+                private val dayOfMonth: TextView = 0
+
+                CalendarViewHolder(private val binding: CalendarCellBinding) {
+                    super(binding.root)
+                }
+        */
+        fun bind(dayOfMonth: String) {
+            binding.calendarCellText.text = dayOfMonth
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
+
+        val cellBinding = DataBindingUtil.inflate<CalendarCellBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.calendar_cell,
+            parent,
+            false
+        )
+
+        val layoutParams: ViewGroup.LayoutParams = cellBinding.root.layoutParams;
+        layoutParams.height = (parent.height * 0.166666).toInt();
+
+        return CalendarViewHolder(cellBinding)
+    }
+
+
+    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
+        daysOfMonth?.let {
+            holder.bind(it[position])
+        }
+    }
+
+    override fun getItemCount() = daysOfMonth?.size ?: 0
 }
